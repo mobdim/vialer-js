e3b350b069affa76664a453af1dd8f6be8ecc6fb
Jeroen van Veen <j.veenvan@gmail.com>
2017-09-13 12:40:01 +0200

    Initial code for proxy state emitter.

--- src/js/lib/skeleton.js
+++ src/js/lib/skeleton.js
@@ -23,53 +23,12 @@ class Skeleton extends EventEmitter {
         this.modules = {}
 
         this.name = options.name
+        this.logger = new Logger(this)
         this.store = new Store(this)
         this.i18n = new I18n(this, options.i18n)
-        this.logger = new Logger(this)
-
-        this._init()
-        // Init these modules.
-        for (let module of options.modules) {
-            this.modules[module.name] = new module.Module(this)
-        }
-
-        // Increases verbosity beyond the logger's debug level.
-        this.verbose = false
-        // Sets the verbosity of the logger.
-        if (process.env.NODE_ENV === 'production') {
-            this.logger.setLevel('warn')
-        } else {
-            this.logger.setLevel('debug')
-        }
 
         if (this.env.extension) {
-            // Make the EventEmitter .on method compatible with
-            // web extension ipc. An Ipc event is coming in. Map it to
-            // the EventEmitter.
-            this.browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-                if (request.data) {
-                    // Add extra contextual information about sender to payload.
-                    request.data.sender = sender
-                    // It may have a callback, but functions can't pass through
-                    // the request.data, so map sendResponse.
-                    request.data.callback = sendResponse
-                }
-                // The frame option can be used to specifically target a
-                // callstatus or observer script. Otherwise the event is
-                // ignored, because otherwise all events emitted on the tab will
-                // also be processed by the callstatus and observer scripts.
-                if (this.env.extension.callstatus || this.env.extension.observer) {
-                    if (this.env.extension.callstatus && request.data.frame && request.data.frame === 'callstatus') {
-                        this.emit(request.event, request.data, true)
-                    } else if (this.env.extension.observer && request.data.frame && request.data.frame === 'observer') {
-                        this.emit(request.event, request.data, true)
-                    }
-                } else {
-                    this.emit(request.event, request.data, true)
-                }
-
-            })
-
+            this.ipcListener()
             // Allows parent scripts to use the same EventEmitter syntax.
             if (this.env.extension.tab) {
                 window.addEventListener('message', (event) => {
@@ -78,6 +37,24 @@ class Skeleton extends EventEmitter {
                 })
             }
         }
+
+        this.stateProxy((state) => {
+            this.state = state
+            this._init()
+            // Init these modules.
+            for (let module of options.modules) {
+                this.modules[module.name] = new module.Module(this)
+            }
+
+            // Increases verbosity beyond the logger's debug level.
+            this.verbose = false
+            // Sets the verbosity of the logger.
+            if (process.env.NODE_ENV === 'production') {
+                this.logger.setLevel('warn')
+            } else {
+                this.logger.setLevel('debug')
+            }
+        })
     }
 
 
@@ -102,15 +79,12 @@ class Skeleton extends EventEmitter {
             }
 
             if (tabId) {
-                this.logger.debug(`${this}emit ipc event '${event}' to tab ${tabId}`)
+                if (this.verbose) this.logger.debug(`${this}emit ipc event '${event}' to tab ${tabId}`)
                 this.browser.tabs.sendMessage(tabId, payloadData)
                 return
             } else if (parent) {
-                this.logger.debug(`${this}emit ipc event '${event}' to parent`)
-                parent.postMessage({
-                    data: data,
-                    event: event,
-                }, '*')
+                if (this.verbose) this.logger.debug(`${this}emit ipc event '${event}' to parent`)
+                parent.postMessage({data: data, event: event}, '*')
                 return
             }
 
@@ -120,13 +94,13 @@ class Skeleton extends EventEmitter {
                 delete data.callback
                 this.browser.runtime.sendMessage(payloadData, callback)
             }
-            this.logger.debug(`${this}emit ipc event '${event}'`)
+            if (this.verbose) this.logger.debug(`${this}emit ipc event '${event}'`)
             this.browser.runtime.sendMessage(payloadData)
         }
         // The web version will always use a local emitter, no matter what
         // the value is of `noIpc`. An extension may do both.
         if (!this.env.extension || noIpc) {
-            this.logger.debug(`${this}emit local event '${event}'`)
+            if (this.verbose) this.logger.debug(`${this}emit local event '${event}'`)
             super.emit(event, data)
         }
     }
@@ -170,9 +144,129 @@ class Skeleton extends EventEmitter {
     }
 
 
+    /**
+    * Make the EventEmitter `.on` method compatible with web extension IPC,
+    * by mapping an IPC event to the EventEmitter.
+    */
+    ipcListener() {
+        this.browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+            if (request.data) {
+                // Add extra contextual information about sender to payload.
+                request.data.sender = sender
+                // It may have a callback, but functions can't pass through
+                // the request.data, so map sendResponse.
+                request.data.callback = sendResponse
+            }
+            // The frame option can be used to specifically target a
+            // callstatus or observer script. Otherwise the event is
+            // ignored, because otherwise all events emitted on the tab will
+            // also be processed by the callstatus and observer scripts.
+            if (this.env.extension.callstatus || this.env.extension.observer) {
+                if (this.env.extension.callstatus && request.data.frame && request.data.frame === 'callstatus') {
+                    this.emit(request.event, request.data, true)
+                } else if (this.env.extension.observer && request.data.frame && request.data.frame === 'observer') {
+                    this.emit(request.event, request.data, true)
+                }
+            } else {
+                this.emit(request.event, request.data, true)
+            }
+        })
+    }
+
+
+    /**
+    * Override the default EventEmitter's `on` method, in
+    * order to keep track of listeners.
+    * @param {String} event - Event name to cache.
+    * @param {Function} callback - The function to call on this event.
+    */
     on(event, callback) {
         this._listeners += 1
         super.on(event, callback)
+    }
+
+
+    /**
+    * The state object is a proxy when using the app in extension mode.
+    * Otherwise, it's just a plain object. As a proxy, it emits each set
+    * action to all IPC listeners, which allows scripts to easily share state,
+    * without having to resort to event emitters for simple state changes.
+    * @param {Function} ready - Called when the state proxy is ready.
+    */
+    stateProxy(ready) {
+        // This is the local state object that's being modified.
+        // Modifying `_state` won't emit changes.
+        let state
+
+        const stateTraps = {
+            get: (target, key) => {
+                if (typeof target[key] === 'object' && target[key] !== null) {
+                    if (key === '_state') {
+                        return target[key]
+                    }
+                }
+                return target._state[key]
+            },
+            set: (obj, key, value) => {
+                // Modifying the raw state object.
+                if (obj === state._state) {
+                    obj[key] = value
+                } else {
+                    // Make sure we are not overwriting the _state property.
+                    if (key === '_state') {
+                        this.logger.warn(`${this}do not try to overwrite the _state property`)
+                    }
+                    // Emit to all listeners, even to the script emitting
+                    // itself, because the property is set in the `app.state`
+                    // event.
+                    let payload = {key, value}
+                    if (obj.name) payload.module = obj.name
+                    this.emit('skeleton:update_state', payload, 'both')
+                }
+                return true
+            },
+        }
+
+        let stateProxy
+
+        this.on('skeleton:update_state', (data) => {
+            if (data.module) stateProxy._state[data.module]._state[data.key] = data.value
+            else stateProxy._state[data.key] = data.value
+        })
+
+        // A new extension app instance will get it's state from the background.
+        if (this.env.extension) {
+            if (this.env.extension.background) {
+                state = {
+                    _state: {
+                        availability: new Proxy({_state: {}, name: 'availability'}, stateTraps),
+                        contacts: new Proxy({_state: {}, name: 'contacts'}, stateTraps),
+                        dialer: new Proxy({_state: {}, name: 'dialer'}, stateTraps),
+                        observer: new Proxy({_state: {}, name: 'observer'}, stateTraps),
+                        queues: new Proxy({_state: {}, name: 'queues'}, stateTraps),
+                        ui: new Proxy({_state: {}, name: 'ui'}, stateTraps),
+                        user: new Proxy({_state: {}, name: 'user'}, stateTraps),
+                    },
+                }
+                stateProxy = new Proxy(state, stateTraps)
+                this.on('skeleton:request_state', (data) => {
+                    data.callback(this.state)
+                })
+                ready(stateProxy)
+            } else {
+                this.emit('skeleton:request_state', {
+                    callback: (initialState) => {
+                        // Rebuild the proxy state from the bg.
+                        for (let moduleName of Object.keys(initialState._state)) {
+                            initialState._state[moduleName] = new Proxy(initialState._state[moduleName], stateTraps)
+                        }
+                        state = initialState
+                        stateProxy = new Proxy(state, stateTraps)
+                        ready(stateProxy)
+                    },
+                })
+            }
+        }
     }
 
 
